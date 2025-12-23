b3f76d6b960f957b2bb530777cd10abefa8cfe6e
Richard Kirk <richkirk922@hotmail.com>
2025-12-23 15:36:02 +0000

    got sf connection working

--- pages/show_snowflake_data.py
+++ pages/show_snowflake_data.py
@@ -3,15 +3,14 @@ import streamlit as st
 st.title("here we display data from a snowflake database")
 
 
-# This works when deployed to Snowflake:
+# This works both when deployed to Snowflake and running locally (locally requires secrets.toml)
 
 conn = st.connection("snowflake")
 
-# df = conn.query("SELECT COL1,COL2 FROM DEV_STREAMLIT_DEMO.SCHEMA1.DAFT_TABLE;", ttl="10m")
-df = conn.query("SELECT COL1,COL2 FROM DAFT_TABLE;", ttl="10m") # Works also without qualifying the the database and schema , think because when deploy the BIC_marketplace connection is passed which includes these
+df = conn.query("SELECT COL1,COL2 FROM DAFT_TABLE;", ttl="10m") # Works without qualifying the the database and schema. These are contained in secrets.toml / connections.toml
 
 for row in df.itertuples():
-    st.write(f"{row.COL1} has a :{row.COL2}:")
+   st.write(f"{row.COL1} has a :{row.COL2}:")
 
 
 
